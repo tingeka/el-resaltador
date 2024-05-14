c38a3db138216d0695ef49b5a135a8dc9aae1eb8
tingeka <53122864+tingeka@users.noreply.github.com>
2024-05-14 20:53:32 -0300

    fix: load-blocks.php clean up

--- theme/gutenberg/load-blocks.php
+++ theme/gutenberg/load-blocks.php
@@ -1,16 +1,5 @@
 <?php
 
-// add_action( 'carbon_fields_register_fields', 'cmlt_er_load_blocks' );
-// function cmlt_er_load_blocks() {
-//     require_once 'nota-de-tapa/block-nota-tapa.php';
-// }
-
-// function minimal_block_ca6eda___register_block() {
-//     var_dump( __DIR__ . '/nota-de-tapa/build' );
-//     register_block_type( __DIR__ . '/build/block.json' );
-// }
-// add_action( 'init', 'minimal_block_ca6eda___register_block' );
-
 function cmlt_er_load_blocks() {
     // Get an array of all block.json files in the directory
     $block_json_files = glob( __DIR__ . '/build/blocks/*/block.json' );
