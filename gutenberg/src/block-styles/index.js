71fcc635eb71f91dc61e06e6a6fa6174ca44f0b6
tingeka <53122864+tingeka@users.noreply.github.com>
2024-07-13 19:52:49 -0300

    chore: clean up of unused block styles

--- gutenberg/src/block-styles/index.js
+++
@@ -1,1 +0,0 @@
-import separatorStyles from './separator-styles/index';
--- gutenberg/src/block-styles/separator-styles/index.js
+++
@@ -1,8 +0,0 @@
-import { registerBlockStyle } from '@wordpress/blocks';
-
-wp.domReady( () => {
-    registerBlockStyle( 'core/separator', {
-        name: 'separator-gradient',
-        label: 'Gradiente',
-    } );
-} );
