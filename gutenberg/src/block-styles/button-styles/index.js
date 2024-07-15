f688fcd25766bdfc1ee1207cf4e146d89eb52906
tingeka <53122864+tingeka@users.noreply.github.com>
2024-07-15 15:35:18 -0300

    feat: new button style and theme.json overrides

---
+++ gutenberg/src/block-styles/button-styles/index.js
@@ -0,0 +1,16 @@
+import { registerBlockStyle, unregisterBlockStyle, unregisterBlockVariation } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+
+
+wp.domReady( () => {
+
+    registerBlockStyle( 'core/button', {
+        name: 'neutral',
+        label: __('Neutral', 'el-resaltador'),
+    } );
+
+    unregisterBlockStyle( 'core/button', 'default' );
+    unregisterBlockStyle( 'core/button', 'outline' );
+    unregisterBlockStyle( 'core/button', 'fill' );
+    
+} );
---
+++ gutenberg/src/block-styles/index.js
@@ -0,0 +1,1 @@
+import { buttonStyles } from './button-styles/index.js';
